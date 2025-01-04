a75943768427dec056a2fc1ebdef65eea7cff52e
Direwen <direwensik@gmail.com>
2025-01-04 14:05:27 +0630

    Added Grid Based Game Board along with drag and drop functions

---
+++ src/components/LaneRow.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="middle-line col-span-1 bg-gray-600"></div>
+    <div class="middle-line col-span-1 bg-white"></div>
+    <div class="middle-line col-span-1 bg-gray-600"></div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full p-2 overflow-x-scroll flex gap-2">
+    <span class="entity text-6xl" id="pedestrian">🚶</span>
+    <span class="entity text-6xl" id="cyclist">🚴</span>
+    <span class="entity text-6xl" id="barrier">🚧</span>
+  </div>
+</template>
