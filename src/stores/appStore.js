dc54b328f5d87164cd573b6758fecdc3d31fa905
Direwen <direwensik@gmail.com>
2025-01-06 23:40:14 +0630

    Added a restriction to require an AV entity and a collision detector

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -17,29 +17,29 @@ export const useAppStore = defineStore('app', {
                     id: "entity-pedestrain",
                     name: "pedestrain",
                     content: "🚶",
-                    societalValue: 0,
-                    speed: 150
+                    societalValue: 1,
+                    speed: 120
                 },
                 "entity-cyclist" : {
                     id: "entity-cyclist",
                     name: "cyclist",
                     content: "🚴",
-                    societalValue: 0,
-                    speed: 150
+                    societalValue: 2,
+                    speed: 200
                 },
                 "entity-av" : {
                     id: "entity-av",
                     name: "av",
                     content: "🚙",
-                    societalValue: 0,
-                    speed: 150
+                    societalValue: 3,
+                    speed: 100
                 },
                 "entity-truck" : {
                     id: "entity-truck",
                     name: "truck",
                     content: "🛻",
-                    societalValue: 0,
-                    speed: 150
+                    societalValue: 2,
+                    speed: 80
                 },
 
             },
@@ -104,6 +104,18 @@ export const useAppStore = defineStore('app', {
                 toast.error("Place an entity first");
                 return;
             }
+
+            // Ensure that if there's an AV entity, it is the last one in placedEntities
+            // Sort to ensure AV is the last entity in placedEntities
+            this.placedEntities.sort((a, b) => {
+                return a.entityId === 'entity-av' ? 1 : b.entityId === 'entity-av' ? -1 : 0;
+            });
+
+            if (this.placedEntities[this.placedEntities.length - 1].entityId != 'entity-av') {
+                toast.error("AV entity must be included");
+                return;
+            }
+
             this.playMode = true;
             this.createBackup();
             this.runEntityMovements();
@@ -150,27 +162,31 @@ export const useAppStore = defineStore('app', {
         
                 this.placedEntities.forEach(entity => {
                     const prevPosition = entity.position;
-                    this.moveGenericEntity(entity);
+                    if (entity.entityId == 'entity-av') this.moveAvEntity(entity, this.placedEntities);
+                    else this.moveGenericEntity(entity);
                     if (entity.position !== prevPosition) {
                         anyEntityMoved = true;
                     }
                 });
         
                 // Reset stuck counter if any entity moves
-                if (anyEntityMoved) {
-                    this.stuckCounter = 0;
-                } else {
-                    this.stuckCounter++;
-                }
+                if (anyEntityMoved) this.stuckCounter = 0; 
+                else this.stuckCounter++;
                 
                 // Stop play mode if no movement for multiple intervals
                 if (this.stuckCounter >= this.maxStuckIntervals) {
                     this.stopPlayMode();
-                    this.showClearButton = true;
                     clearInterval(moveInterval);  // Stop the interval properly
                 }
-        
-                this.detectCurrentCollisions();
+
+                // Stop play mode of an collision occured
+                const avCollisionPosition = this.detectAvCollisions(this.placedEntities);
+                if (avCollisionPosition >= 0) {
+                    toast.error(`A Collision occured at ${avCollisionPosition}`)
+                    this.stopPlayMode();
+                    clearInterval(moveInterval);  // Stop the interval properly
+                }
+
             }, 50);
         },
 
@@ -180,7 +196,20 @@ export const useAppStore = defineStore('app', {
                 this.updatePosition(placedEntity);
                 placedEntity.lastMoveTime = Date.now();  // Update the placed entity's movement time
             }
-        },        
+        },
+        
+        moveAvEntity(placedEntity, placedEntities) {
+            const entityDetails = this.entities[placedEntity.entityId];
+            if (this.istimeToMove(placedEntity, entityDetails)) {
+                // Predict Potential Collisions
+                this.predictCollisions(placedEntity, placedEntities);
+                //Avoid if possible
+                // If unavoidable, stop (skip the turn to move)
+                // If not stopptable, 
+                this.updatePosition(placedEntity);
+                placedEntity.lastMoveTime = Date.now();  // Update the placed entity's movement time
+            }
+        },
         
         istimeToMove(placedEntity, entityDetails) {
             return !placedEntity.lastMoveTime || Date.now() - placedEntity.lastMoveTime >= entityDetails.speed;
@@ -211,8 +240,37 @@ export const useAppStore = defineStore('app', {
             }
         },
 
-        detectCurrentCollisions() {
-            
-        }
+        predictNextPosition(placedEntity) {
+            let nextPosition = placedEntity.position;
+
+            switch (placedEntity.direction) {
+                case 'up': nextPosition -= this.totalColumns; break;
+                case 'down': nextPosition += this.totalColumns; break;
+                case 'left': nextPosition -= 1; break;
+                case 'right': nextPosition += 1; break;
+            }
+
+            return nextPosition;
+        },
+
+        detectAvCollisions(placedEntities) {
+            const avPosition = placedEntities[placedEntities.length - 1].position;
+
+            for (const entity of placedEntities) {
+                if (entity.entityId != 'entity-av') {
+                    if (entity.position == avPosition) return avPosition; 
+                }
+            }
+
+            return -1;  // Return -1 if no collision is detected
+        },
+
+        predictCollisions(mainEntity, placedEntities) {
+            const filteredEntities = placedEntities.filter(entity => entity.entityId != mainEntity.entityId);
+            filteredEntities.forEach(entity => {
+                
+            })
+        },
+        
     }
 })
