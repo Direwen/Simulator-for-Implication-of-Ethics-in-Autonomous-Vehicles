1c11769c6df99e0d14e370b31ea8e59be6604432
Direwen <direwensik@gmail.com>
2025-01-14 10:08:53 +0630

    Added a conditional display in Modal

--- src/components/EntityModal.vue
+++ src/components/EntityModal.vue
@@ -1,7 +1,5 @@
 <template>
-
     <ModalTemplate>
-
         <template #header>
             <section class="text-3xl md:text-4xl lg:text-6xl animate-bounce text-center">
                 {{ entityDetails?.content }}
@@ -15,27 +13,58 @@
                     <span>Name</span>
                     <span>{{ entityDetails?.name }}</span>
                 </div>
-                <div class="flex justify-between items-center font-semibold uppercase">
+
+                <!-- Conditionally show Societal Value -->
+                <div 
+                  v-if="entityDetails?.societalValue !== Infinity" 
+                  class="flex justify-between items-center font-semibold uppercase"
+                >
                     <span>Societal Value</span>
-                    <input type="number" min="0" max="100" :value="entityDetails?.societalValue" @input="updateSocietalValue($event.target.value)">
+                    <input
+                        type="number"
+                        min="0"
+                        max="100"
+                        :value="entityDetails?.societalValue"
+                        @input="updateSocietalValue($event.target.value)"
+                    >
                 </div>
-                <div class="flex justify-between items-center font-semibold uppercase">
+
+                
+
+                <!-- Conditionally show Speed -->
+                <div 
+                  v-if="entityDetails?.canMove" 
+                  class="flex justify-between items-center font-semibold uppercase"
+                >
                     <span>Speed</span>
-                    <input type="number" min="50" max="500" :value="entityDetails?.speed" @input="updateSpeed($event.target.value)" class="max-w-1/4 bg-transparent border px-2 py-1 rounded">
+                    <input
+                        type="number"
+                        min="50"
+                        max="500"
+                        :value="entityDetails?.speed"
+                        @input="updateSpeed($event.target.value)"
+                        class="max-w-1/4 bg-transparent border px-2 py-1 rounded"
+                    >
+                </div>
+
+                <div v-else>
+                    <p class="text-red-700 text-sm lg:text-base text-left tracking-tighter">Notice: This Entity is considered an object which is the least important among entities.</p>
                 </div>
             </section>
             <div class="divider"></div>
         </template>
 
         <template #footer>
-            <button v-if="cellIndex" class="btn btn-error"
-                @click="appStore.removeEntity(cellIndex); appStore.closeModal();">Remove</button>
+            <button 
+              v-if="cellIndex" 
+              class="btn btn-error"
+              @click="appStore.removeEntity(cellIndex); appStore.closeModal();"
+            >
+                Remove
+            </button>
         </template>
-
     </ModalTemplate>
-
 </template>
-
 
 <script setup>
 import { onMounted, ref } from 'vue';
@@ -49,23 +78,31 @@ const entityDetails = ref(null);
 onMounted(() => {
     if (props.id) {
         entityDetails.value = appStore.entities[props.id];
+        console.log(entityDetails.value);
         return;
     }
-    const placedEntity = appStore.placedEntities.filter(en => en.position == props.cellIndex);
+    const placedEntity = appStore.placedEntities.filter(
+        (en) => en.position == props.cellIndex
+    );
     entityDetails.value = appStore.entities[placedEntity[0].id];
+    console.log(entityDetails.value.societalValue);
 });
 
 const updateSocietalValue = (value) => {
     const clampedValue = Math.max(0, Math.min(100, value));
     entityDetails.value.societalValue = clampedValue;
-    const placedEntity = appStore.placedEntities.find(en => en.position == props.cellIndex);
+    const placedEntity = appStore.placedEntities.find(
+        (en) => en.position == props.cellIndex
+    );
     appStore.entities[placedEntity.id].societalValue = clampedValue;
 };
 
 const updateSpeed = (value) => {
     const clampedValue = Math.max(50, Math.min(500, value));
     entityDetails.value.speed = clampedValue;
-    const placedEntity = appStore.placedEntities.find(en => en.position == props.cellIndex);
+    const placedEntity = appStore.placedEntities.find(
+        (en) => en.position == props.cellIndex
+    );
     appStore.entities[placedEntity.id].speed = clampedValue;
 };
 </script>
